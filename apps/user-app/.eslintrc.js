e67eb688f6f8bf5d227907983833af687f0c44c3
kcaakash49 <kcaakash4910@gmail.com>
2024-07-30 15:46:34 +0545

    --init

--- apps/user-app/.eslintrc.js
+++ apps/user-app/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   root: true,
   extends: ["@repo/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
+  "exclude": ["node_modules", "tailwind.config.js"],
   parserOptions: {
     project: true,
   },
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@repo/db/client";
+
+export default function Home() {
+  return (
+    <div className="text-2xl text-red-200">
+      Hello
+    </div>
+  );
+}
